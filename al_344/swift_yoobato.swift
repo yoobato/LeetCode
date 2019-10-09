d7a143a51581444b145fe846f995f456c2411294
Daeyeol Ryu <dy@yoobato.com>
2019-10-09 16:31:20 +0900

    Update swift_yoobato.swift
    Updated answer with Swift 4.2

--- al_344/swift_yoobato.swift
+++ al_344/swift_yoobato.swift
@@ -3,14 +3,19 @@
 // https://leetcode.com/problems/reverse-string/
 
 class Solution {
-    func reverseString(s: String) -> String {
-        var reversedString: String = ""
-        for r in s.characters {
-            reversedString = "\(r)" + reversedString
+    func reverseString(_ s: inout [Character]) {
+        var left = 0
+        var right = s.count - 1
+        while left < right {
+            // Swap
+            (s[left], s[right]) = (s[right], s[left])
+            left += 1
+            right -= 1
         }
-        return reversedString
     }
 }
 
 // For test
-//print(Solution().reverseString("hello"))
+//var s: [Character] = ["h", "e", "l", "l", "o"]
+//Solution().reverseString(&s)
+//print(s)
